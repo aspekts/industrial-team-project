81dede648ed24147c688ac11705fb54d5d5f64d1
Maxbxy3003 <maxboey3@gmail.com>
2026-03-26 01:32:19 +0000

    Update test_ingest.py
    changed import ingest to
    from src.parsers import ingest

--- tests/parsers/test_ingest.py
+++ tests/parsers/test_ingest.py
@@ -14,7 +14,7 @@ import sys
 # ──────────────────────────────────────────────────────────────────────────────
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-import ingest
+from src.parsers import ingest
 
 
 # ──────────────────────────────────────────────────────────────────────────────
